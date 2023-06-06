377af830d135179b76a65be088ed1c4dfcef4adc
Akas7017 <akash.verma@walkingtree.tech>
2023-06-06 10:46:54 +0530

    airbyte

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,17 +11,30 @@
 
 {{ config(materialized='table') }}
 
+-- with wtt_customerassetsreading as (
+--     select 
+--         value,
+--         id,
+--         sort,
+--         readingdatetime,
+--         assetid,
+--         _airbyte_ab_id,
+--         _airbyte_emitted_at,
+--         _airbyte_normalized_at,
+--         _airbyte_wtt_customerassetsreading_hashid
+--     from airbytepoc.wtt_customerassetsreading
+--     limit 3
+-- )
+
+-- select *
+-- from wtt_customerassetsreading
+
+
+
+
+
 with wtt_customerassetsreading as (
-    select 
-        value,
-        id,
-        sort,
-        readingdatetime,
-        assetid,
-        _airbyte_ab_id,
-        _airbyte_emitted_at,
-        _airbyte_normalized_at,
-        _airbyte_wtt_customerassetsreading_hashid
+    select *
     from airbytepoc.wtt_customerassetsreading
     limit 3
 )
